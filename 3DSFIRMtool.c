3d8cbd803d9e66f88cf5be0d9944f56325bb932c
d0k3 <tore.anon@gmail.com>
2016-05-08 15:02:30 +0200

    Partial rewrite, make this work like users expect
    o now properly checks for actual FIRM size
    o dumps actual FIRM size
    o concatenated files are no more supported

--- 3DSFIRMtool.c
+++ 3DSFIRMtool.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE_CTR_FIRM   0x000F3000
 #define OFF_CTR_FIRM0   0x0B130000
 #define OFF_CTR_FIRM1   0x0B530000
 
@@ -12,9 +11,9 @@
 
 
 void showhelp_exit() {
-    printf(" usage: 3DSFIRMtool [-d|-i] [NAND] [FIRM0FIRM1] [XORPAD]\n");
-    printf("  -d   Dump FIRM0/FIRM1 from NAND file\n");
-    printf("  -i   Inject FIRM0/FIRM1 to NAND file\n");
+    printf(" usage: 3DSFIRMtool [-d?|-i?] [NAND] [FIRM0|FIRM1] [XORPAD]\n");
+    printf("  -d/-d0/-d1   Dump FIRM0/FIRM1 from NAND file\n");
+    printf("  -i/-i0/-i1   Inject FIRM0/FIRM1 to NAND file\n");
     exit(0);
 }
 
@@ -26,25 +25,28 @@ int main( int argc, char** argv )
     unsigned char* bufenc;
     unsigned char* bufxor;
     int dump;
+    int firmn;
     
-    size_t tsize_f0;
-    size_t tsize_f1;
-    
+    size_t tsize_firm;
     size_t offset;
     size_t size;
     
-    printf("\n3DSFIRMtool (C version) by d0k3\n");
-    printf("-------------------------------\n\n");
+    printf("\n3DSFIRMtool (v0.2) by d0k3\n");
+    printf("--------------------------\n\n");
     
     if(argc < 5) showhelp_exit();
-    if(strcmp(argv[1], "-d") == 0) dump = 1;
-    else if(strcmp(argv[1], "-i") == 0) dump = 0;
+    if(strlen(argv[1]) > 3) showhelp_exit();
+    if(strncmp(argv[1], "-d", 2) == 0) dump = 1;
+    else if(strncmp(argv[1], "-i", 2) == 0) dump = 0;
+    else showhelp_exit();
+    if (argv[1][2] == '0' || !argv[1][2]) firmn = 0;
+    else if (argv[1][2] == '1') firmn = 1;
     else showhelp_exit();
     
     if(dump) {
-        printf("dumping & decrypting %s\n from %s\n using %s\n\n", argv[3], argv[2], argv[4]);
+        printf("dumping & decrypting %s\n from %s (firm%i)\n using %s\n\n", argv[3], argv[2], firmn, argv[4]);
     } else {
-        printf("injecting & encrypting %s\n to %s\n using %s\n\n", argv[3], argv[2], argv[4]);
+        printf("injecting & encrypting %s\n to %s (firm%i)\n using %s\n\n", argv[3], argv[2], firmn, argv[4]);
     }
     
     fp_xor = fopen(argv[4], "rb");
@@ -57,12 +59,18 @@ int main( int argc, char** argv )
     fseek(fp_xor, 0, SEEK_END);
     size = ftell(fp_xor);
     fseek(fp_xor, 0, SEEK_SET);
-    if (size == XORPAD_SIZE) { // FIRM xorpad
-        offset = OFF_CTR_FIRM0;
-    } else {
+    if (size != XORPAD_SIZE) { // FIRM xorpad
         printf("xorpad has bad size!\n\n");
         return 0;
-    }        
+    }
+
+    // adapt for FIRM0 or FIRM1
+    if (firmn == 0) { // FIRM0
+        offset = OFF_CTR_FIRM0;
+    } else { // FIRM1
+        offset = OFF_CTR_FIRM1;
+        fseek(fp_xor, FIRM_SIZE, SEEK_SET);
+    }
     
     if(dump) {
         fp_in = fopen(argv[2], "rb");
@@ -88,6 +96,17 @@ int main( int argc, char** argv )
             return 0;
         }
         fseek(fp_out, offset, SEEK_SET);
+        // check input firm size
+        fseek(fp_in, 0, SEEK_END);
+        tsize_firm = ftell(fp_in);
+        if (tsize_firm > FIRM_SIZE) {
+            printf("%s is too big!\nUse 3DSFAT16tool to write concatenated firms\n\n", argv[2]);
+            return 0;
+        } else if (tsize_firm < 512) {
+            printf("%s is too small!\n\n", argv[2]);
+            return 0;
+        }
+        fseek(fp_in, 0, SEEK_SET);
     }
     
     bufenc = (unsigned char*) malloc(XORPAD_SIZE);
@@ -98,38 +117,40 @@ int main( int argc, char** argv )
     }
     
     // read input data
-    fread(bufenc, 1, XORPAD_SIZE, fp_in);
+    fread(bufenc, 1, FIRM_SIZE, fp_in);
+    fread(bufxor, 1, FIRM_SIZE, fp_xor);
     
-    // find firm0firm1 true size
-    for (tsize_f0 = FIRM_SIZE; tsize_f0 > 0; tsize_f0--)
-        if (bufenc[tsize_f0 - 1] != 0x00) break;
-    if (tsize_f0 % UNIT_SIZE) tsize_f0 += UNIT_SIZE - (tsize_f0 % UNIT_SIZE);
-    for (tsize_f1 = FIRM_SIZE; tsize_f1 > 0; tsize_f1--)
-        if (bufenc[FIRM_SIZE + tsize_f1 - 1] != 0x00) break;
-    if (tsize_f1 % UNIT_SIZE) tsize_f1 += UNIT_SIZE - (tsize_f1 % UNIT_SIZE);
-    printf("FIRM0 true size: %X\nFIRM1 true size: %X\n", tsize_f0, tsize_f1);
-    
-    // read and prepare xorpad
-    memset(bufxor, 0x00, XORPAD_SIZE);
-    fseek(fp_xor, 0, SEEK_SET);
-    fread(bufxor, 1, tsize_f0, fp_xor);
-    fseek(fp_xor, FIRM_SIZE, SEEK_SET);
-    fread(bufxor + FIRM_SIZE, 1, tsize_f1, fp_xor);
-    
-    // apply xorpad, check for differences
+    // apply xorpad
     printf("processing... ");
     if (!dump) { // when injecting: check, then apply
-        if (memcmp(bufenc, bufenc + FIRM_SIZE, FIRM_SIZE) != 0)
-            printf("\nWARNING: FIRM0 & FIRM1 do not match!\n");
-        for (size_t i = 0; i < XORPAD_SIZE; i++)
+        if (memcmp(bufenc, "FIRM", 4) != 0) {
+            printf("\nnot a proper firm file, won't inject!\n\n");
+            return 0;
+        }
+        for (size_t i = 0; i < tsize_firm; i++)
             bufenc[i] = bufenc[i]^bufxor[i];
-    } else { // when dumping: apply, then check
-        for (size_t i = 0; i < XORPAD_SIZE; i++)
+    } else { // when dumping: apply, then get size / check
+        for (size_t i = 0; i < 512; i++)
             bufenc[i] = bufenc[i]^bufxor[i];
-        if (memcmp(bufenc, bufenc + FIRM_SIZE, FIRM_SIZE) != 0)
-            printf("\nWARNING: FIRM0 & FIRM1 do not match!\n");
+        tsize_firm = 0;
+        for (int section = 0; section < 4; section++) {
+            size_t f_offs = *(int*) (bufenc + 0x40 + 0x00 + (0x30*section));
+            size_t f_size = *(int*) (bufenc + 0x40 + 0x08 + (0x30*section));
+            if (!f_size) continue;
+            if (f_offs + f_size >= tsize_firm) tsize_firm = f_offs + f_size;
+            else {
+                tsize_firm = 0;
+                break;
+            }
+        }
+        if ((memcmp(bufenc, "FIRM", 4) != 0) || !tsize_firm) {
+            printf("\nbad xorpad or corrupt nand.bin, won't dump!\n\n", (char*) bufenc, tsize_firm);
+            return 0;
+        }
+        for (size_t i = 512; i < tsize_firm; i++)
+            bufenc[i] = bufenc[i]^bufxor[i];
     }
-    fwrite(bufenc, 1, XORPAD_SIZE, fp_out);
+    fwrite(bufenc, 1, tsize_firm, fp_out);
     printf("done!\n\n");
     
     free(bufenc);
